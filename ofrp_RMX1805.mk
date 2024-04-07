b85208f146bc2c61c2de6aed29703c40137c1d30
LinuxGuy312 <101087324+LinuxGuy312@users.noreply.github.com>
2024-04-07 19:28:18 +0530

    Update ofrp_RMX1805.mk

--- ofrp_RMX1805.mk
+++ ofrp_RMX1805.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+PRODUCT_RELEASE_NAME = RMX1805
+
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
@@ -27,8 +29,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore=msm8953
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := RMX1805
-PRODUCT_NAME := ofrp_RMX1805
+PRODUCT_DEVICE := $(PRDUCT_RELEASE_NAME)
+PRODUCT_NAME := ofrp_$(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := Oppo
 PRODUCT_MODEL := Realme 2
 PRODUCT_MANUFACTURER := Oppo
