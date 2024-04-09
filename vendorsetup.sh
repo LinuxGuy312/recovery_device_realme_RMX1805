cdc4428f1a2cfb083eb9e78f5ca0ea82f9b5a1bb
LinuxGuy312 <101087324+LinuxGuy312@users.noreply.github.com>
2024-04-09 14:10:43 +0530

    Add avatar

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,12 +17,15 @@
 # This file is executed by build/envsetup.sh, and can use anything
 # defined in envsetup.sh.
 
+wget -qO $(pwd)/OFoxAvatar.png https://i.postimg.cc/cCgwfTqh/OFox-Avatar.png
+
 export TARGET_DEVICE_ALT="RMX1809, RMX1811" \
        FOX_DELETE_AROMAFM=1 \
        FOX_VERSION=R11.1 \
        FOX_TARGET_DEVICES="RMX1809,RMX1811" \
        FOX_ENABLE_APP_MANAGER=1 \
        OF_MAINTAINER="Eren (@WH0907)" \
+       OF_MAINTAINER_AVATAR="$(pwd)/OFoxAvatar.png"
        OF_ALLOW_DISABLE_NAVBAR=0 \
        OF_ENABLE_LPTOOLS=1 \
        OF_CLOCK_POS=1 \
@@ -35,3 +38,4 @@ export TARGET_DEVICE_ALT="RMX1809, RMX1811" \
 latest_tag=$(curl -s https://api.github.com/repos/topjohnwu/Magisk/releases/latest | grep "tag_name" | sed -E 's/.*"([^"]+)".*/\1/');\
 nonv=${latest_tag:1:10};mkdir ~/Magisk;wget -O ~/Magisk/Magisk-$nonv.zip https://github.com/topjohnwu/Magisk/releases/download/$latest_tag/Magisk-$latest_tag.apk --show-progress;\
 export FOX_USE_SPECIFIC_MAGISK_ZIP="~/Magisk/Magisk-$nonv.zip"
+
